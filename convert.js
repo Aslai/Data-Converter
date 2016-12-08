38d9acd2b980181e41a979572df44cff9f2981d1
Andrew Story <kaslai@kaslai.com>
2016-12-07 16:25:39 -0800

    Add exp golomb and 2's complement numbers

--- convert.js
+++ convert.js
@@ -58,12 +58,20 @@ function update(){
     document.getElementById("conv_out").value = data;
 }
 
+function updatecfg(dest){
+    for( var i = 0; i < dest.children.length; ++i ){
+        dest.children[i].onload();
+    }
+}
 
 autoupdate = function(){
+    updatecfg(document.getElementById("opts_in"));
+    updatecfg(document.getElementById("opts_out"));
     if( document.getElementById("autoUpdate").checked == true ){
         update();
     }
 }
+
 
 document.getElementById("conv_in").onchange = autoupdate;
 document.getElementById("conv_in").onkeyup = autoupdate;
@@ -71,13 +79,64 @@ document.getElementById("conv_in").onkeyup = autoupdate;
 state.notice = function( msg ){
     document.getElementById("diagnostics").innerHTML += msg + "\n";
 }
-
+function dumpenableif( obj, dest, item ){
+    if( item.enable_if != null ){
+        dest.onload = (function(obj, dest, item){
+			return function(){
+                var isTrue = false;
+                for(var idx in item.enable_if ){
+                    var matches = 0;
+                    var total = 0;
+                    for( var prop in item.enable_if[idx] ){
+                        if( !item.enable_if[idx].hasOwnProperty(prop) ) {
+                            continue;
+                        }
+                        var matched = false;
+                        for( var check in item.enable_if[idx][prop] ){
+                            if( obj[prop] == item.enable_if[idx][prop][check] ){
+                                matched = true;
+                                break;
+                            }
+                        }
+                        total ++;
+                        if( matched ){
+                            matches++;
+                        }
+                    }
+                    if( total == matches ){
+                        isTrue = true;
+                        break;
+                    }
+                }
+                if( isTrue ){
+                    dest.style.display = "block";
+                }
+                else{
+                    dest.style.display = "none";
+                }
+            }
+        })(obj, dest, item);
+    }
+    else{
+        dest.onload = function(){};
+    }
+}
 
 function dumpcfgcombo( dest, obj, item ){
 	var select = document.createElement("select");
-    select.className = "float_r";
+    select.className = "float_opt";
 	var data = [];
 	var index = 0;
+	dest.appendChild(select);
+	for( var idx in item.options ){
+		var opt = item.options[idx];
+		var option = document.createElement("option");
+		option.value = index++;
+		data.push(opt.set);
+		option.text = opt.name;
+		select.appendChild(option);
+	}
+    select.value = item.selected;
 	select.onchange = (function(obj, item, data, select){
 			return function(){
 				var arr = data[select.value];
@@ -91,23 +150,13 @@ function dumpcfgcombo( dest, obj, item ){
                 autoupdate();
 			}
 		})(obj, item, data, select);
-	dest.appendChild(select);
-	for( var idx in item.options ){
-		var opt = item.options[idx];
-		var option = document.createElement("option");
-		option.value = index++;
-		data.push(opt.set);
-		option.text = opt.name;
-		select.appendChild(option);
-	}
-    select.value = item.selected;
     select.onchange();
     return select;
 }
 function dumpcfgtext( dest, obj, item ){
 	var txt = document.createElement("input");
     txt.type = "text";
-    txt.className = "float_r";
+    txt.className = "float_opt";
 	txt.onchange = (function(obj, item, txt){
 			return function(){
                 obj[item.set] = txt.value;
@@ -115,6 +164,7 @@ function dumpcfgtext( dest, obj, item ){
                 autoupdate();
 			}
 		})(obj, item, txt);
+    txt.onkeyup = txt.onchange;
 	dest.appendChild(txt);
     txt.value = item.selected;
     txt.onchange();
@@ -127,6 +177,7 @@ function dumpcfg(dest, obj){
 	for( var idx in obj.config ){
 		var item = obj.config[idx];
 		var div = document.createElement("div");
+        div.className = "opt";
 		var txt = document.createTextNode(item.name);
 		div.appendChild(txt);
 		if( item.type == "combo" ){
@@ -135,7 +186,10 @@ function dumpcfg(dest, obj){
 		if( item.type == "text" ){
 			dumpcfgtext( div, obj, item );
 		}
+        dumpenableif(obj, div, item);
+        div.onload();
 		dest.appendChild(div);
 	}
 }
+
 
--- convert/number.js
+++ convert/number.js
@@ -1,19 +1,40 @@
 conversions["number"] = function(){
     this.base = 10;
+    this.width = 32;
     this.placeholder = "1234567 ...";
+    this.fmt = 0;
     this.config = [
         { 
             name: "Base:",
             type: "text",
             selected: 10,
             set: "base"
-        }
+        },
+        { 
+            name: "Bin Format:",
+            type: "combo",
+            selected: 0,
+            options: [
+                { name: "Standard Base 2", set: { fmt: 0 } },
+                { name: "2's Complement", set: { fmt: 3 } },
+                { name: "Exp. Golomb", set: { fmt: 1 } },
+                { name: "Signed Exp. Golomb", set: { fmt: 2 } },
+            ]
+        },
+        { 
+            name: "Width:",
+            type: "text",
+            selected: 32,
+            set: "width",
+            enable_if: [{fmt: [3]}]
+        },
+        
     ];
     
     this.toBin = function ( str ){
         var str2 = "";
-        var alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        alphabet = alphabet.substr(0, this.base);
+        var alphabet = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        alphabet = alphabet.substr(0, this.base + 1);
         for( var i = 0; i < str.length; ++i ){
             if( alphabet.indexOf(str[i].toUpperCase()) >= 0 ){
                 str2 += str[i].toUpperCase();
@@ -22,11 +43,52 @@ conversions["number"] = function(){
                 state.invalidInput(null, str[i], i);
             }
         }
-        var out = bigInt(str2, this.base).toString(2);
-        if( out.length % 8 != 0 ){
-            return Array(9 - out.length % 8).join("0") + out;
+        var out = bigInt(str2, this.base);
+        if( this.fmt == 0 ){ 
+            out = out.toString(2);
+            return out;
         }
-         return out;
+        if( this.fmt == 3 ){ 
+            if( out.isNegative() ){
+                out = out.abs().minus(1).toString(2);
+                if( out.length >= this.width ){
+                    state.invalidInput("Overflow", 1, 1);
+                    out = out.substr(out.length - this.width);
+                }
+                else{
+                    out = Array(parseInt(this.width) + 1 - out.length).join("0") + out;
+                }
+                var out2 = "";
+                for( var i = 0; i < out.length; ++i ){
+                    if( out[i] == "0" ){
+                        out2 += "1";
+                    }
+                    else{
+                        out2 += "0";
+                    }
+                }
+                return out2;
+            }
+            out = out.toString(2);
+            if( out.length >= this.width ){
+                state.invalidInput("Overflow", 1, 1);
+                return out.substr(out.length - this.width);
+            }
+            return Array(parseInt(this.width) + 1 - out.length).join("0") + out;
+        }
+        if( this.fmt == 1 ){
+            out = out.add(1).toString(2);
+            return Array(out.length).join("0") + out;
+        }
+        if( this.fmt == 2 ){
+            var add = 0;
+            if( out.isNegative() || out.isZero() ){
+                add = 1;
+            }
+            out = out.abs().multiply(2).add(add).toString(2);
+            return Array(out.length).join("0") + out;
+        }
+        
     };
 
     this.fromBin = function ( str ){
@@ -34,14 +96,64 @@ conversions["number"] = function(){
         var temp = "";
         var i;
         var num = bigInt(str, this.base);
+        var found = false;
+        var leading = 0;
         for( i = 0; i < str.length; ++i ){
-            if( str[i] == "0" || str[i] == "1" ){
+            if( str[i] == "0" ){
                 temp += str[i];
+                if( !found ){
+                    leading ++;
+                }
+            }
+            else if( str[i] == "1" ){
+                temp += str[i];
+                found = true;
             }
             else{
                 state.invalidInput("Non-binary data.", str[i], i);
             }
         }
-        return bigInt(temp, 2).toString(this.base);
+        if( this.fmt == 0 ){ 
+            return bigInt(temp, 2).toString(this.base);
+        }
+        if( this.fmt == 3 ){ 
+            temp = temp.substr(0, this.width);
+            if( temp.length < this.width ){
+                state.invalidInput("Insufficient data", 1, 1);
+            }
+            if( temp[0] == "0" ){
+                return bigInt(temp, 2).toString(this.base);
+            }
+            var out2 = "";
+            for( var i = 0; i < temp.length; ++i ){
+                if( temp[i] == "0" ){
+                    out2 += "1";
+                }
+                else{
+                    out2 += "0";
+                }
+            }
+            return bigInt(out2, 2).add(1).multiply(-1).toString(this.base);
+        }
+        if( this.fmt == 1 ){
+            temp = temp.substr(leading, leading + 1);
+            if( temp.length < leading + 1 ){
+                state.invalidInput("Truncated exponential golomb code", "", str.length);
+            }
+            return bigInt(temp, 2).subtract(1).toString(this.base);
+        }
+        if( this.fmt == 2 ){
+            temp = temp.substr(leading, leading + 1);
+            if( temp.length < leading + 1 ){
+                state.invalidInput("Truncated exponential golomb code", "", str.length);
+            }
+            var ret = bigInt(temp, 2).subtract(1);
+            var remainder = ret.isEven() ? 0 : 1;
+            ret = ret.divide(2).add(remainder);
+            if( remainder == 0 ){
+                ret.multiply(-1);
+            }
+            return ret.toString(this.base);
+        }
     };
 }
